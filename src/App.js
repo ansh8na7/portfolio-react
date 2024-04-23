90e3adbc4a02b8dfffb79c8d9b146ee6be17aca6
Harshvardhan Shete <harshvardhangithub@gmail.com>
2024-04-23 17:45:29 +0530

    react-particles-js is replaced by @tsparticles/react
    1. React version updated from 17.0.2 to 18.2.0
    2. react-particles-js is replaced by @tsparticles/react and code updated as per documentation.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./App.css";
-import Particles from "react-particles-js";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import AboutMe from "./components/AboutMe";
@@ -14,29 +16,82 @@ import Contacts from "./components/Contacts";
 import Footer from "./components/Footer";
 
 function App() {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <>
-      <Particles
-        className="particles-canvas"
-        params={{
-          particles: {
-            number: {
-              value: 30,
-              density: {
+      {init && (
+        <Particles
+          id="tsparticles"
+          className="particles-canvas"
+          particlesLoaded={particlesLoaded}
+          options={{
+            fullScreen: {
+              enable: false,
+            },
+            fpsLimit: 120,
+
+            particles: {
+              color: {
+                value: "#f9ab00",
+              },
+              links: {
+                color: "#ffffff",
+                distance: 150,
                 enable: true,
-                value_area: 900,
+                opacity: 0.5,
+                width: 1,
+              },
+              collisions: {
+                enable: true,
+              },
+              move: {
+                direction: "none",
+                enable: true,
+                outModes: {
+                  default: "bounce",
+                },
+                random: false,
+                speed: 3,
+                straight: false,
+              },
+              number: {
+                density: {
+                  enable: true,
+                  area: 800,
+                },
+                value: 80,
+              },
+              opacity: {
+                value: 0.5,
+              },
+              shape: {
+                type: "circle",
+              },
+              size: {
+                value: { min: 1, max: 5 },
               },
             },
-            shape: {
-              type: "circle",
-              stroke: {
-                width: 6,
-                color: "#f9ab00",
-              },
-            },
-          },
-        }}
-      />
+            detectRetina: true,
+          }}
+        />
+      )}
       <Navbar />
       <Header />
       <AboutMe />
